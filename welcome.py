2b9e72a94f20c66b801356e650f48b60ad9a7e43
Joemamasahbsdh <99223005+Joemamasahbsdh@users.noreply.github.com>
2022-02-16 09:09:56 +1300

    Created component file and welcome message
    Welcome component created - version 1

--- welcome.py
+++ welcome.py
@@ -1,4 +1,3 @@
-# Pizza bot program
 import random 
 from random import randint          
 
@@ -9,4 +8,6 @@ num = randint(0,9)
 
 name = (names[num])
 
-print(name) 
+print("*** Welcome to Dream Pizza***")
+print("*** My name is",name, "***")
+print("***I will be here to help you order your delicious Dream Pizza***")
