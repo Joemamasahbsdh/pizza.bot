727c07554242bf86c40e0f74ebb1922d97336deb
Joemamasahbsdh <99223005+Joemamasahbsdh@users.noreply.github.com>
2022-02-17 23:35:20 +1300

    Added comment for each section
    Comments for each section

--- Pizza._bot.py
+++ Pizza._bot.py
@@ -1,12 +1,70 @@
 # Pizza bot program
-import random 
-from random import randint          
 
-# list of random names
-names = ["Louis", "Gian", "Mikara", "Joaquin", "Tomboy", "BigBen", "Calicdan", "Barlo", "Yacob", "Marcas"]
+# welcome message with randome name
 
-num = randint(0,9)
 
-name = (names[num])
 
-print(name) 
+
+
+
+# Menu for pickup or deliver
+
+
+
+
+
+
+# Pick up information - name and phone number
+
+
+
+
+
+
+# Delivery information - name address and phone
+
+
+
+
+
+# Choose total number Pizzas - max 5
+
+
+
+
+
+
+# Pizza menu
+
+
+
+
+
+
+# PIzza ordering - from menu - print each pizza ordered with cost
+
+
+
+
+
+# Print order out - including if order is del or pick up and names and price of each pizza - total cost including any delivery charge
+
+
+
+
+# Ability to cancel or proceed with order 
+
+
+
+
+
+# Option for new order or to exit
+
+
+
+
+
+
+
+# Main function
+ 
