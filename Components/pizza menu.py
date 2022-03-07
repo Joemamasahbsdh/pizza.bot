69e90d73f397acf38d11b89f1884dcb7aa22f7c4
Joemamasahbsdh <99223005+Joemamasahbsdh@users.noreply.github.com>
2022-03-07 14:41:25 +1300

    Added version 1 pizza_menu
    Created a basic pizza menu

---
+++ Components/pizza menu.py
@@ -0,0 +1,20 @@
+pizza_names = ['Margherita','Pepperoni','Hawaiian','Cheese','Italian','Vegie','Vegan',
+                'Chicken Deluxe','Mega Meat Lovers','Seafood Deluxe','Apricot Chicken Deluxe','BBQ Chicken Deluxe'] 
+
+pizza_prices = [8.50, 8.50, 8.50, 8.50, 8.50, 8.50, 8.50, 13.50, 13.50, 13.50, 13.50, 13.50]
+
+number_pizas = 12
+
+#print("How many pizzas would you like to order?")
+#num_pizza = int(input())
+
+for count in range (number_pizas):
+    print (pizza_names[count],pizza_prices[count])
+
+
+
+ 
+ 
+ 
+ 
+ 
